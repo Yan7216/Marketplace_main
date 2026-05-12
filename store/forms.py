79ae86312d5eb9a32ed8488921cc58c748612343
Yan7216 <aguilaryan040@gmail.com>
2026-05-11 17:56:27 -0700

    Sprint 3

--- store/forms.py
+++ store/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from .models import Product
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
@@ -9,3 +11,11 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'is_seller', 'password1', 'password2')
+       
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'stock', 'categories']
+        widgets = {
+            'categories': forms.CheckboxSelectMultiple()
+        }
